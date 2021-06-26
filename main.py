155dac65650035c13cd35db242b756b86b25aff6
RalXYZ <RalXYZ@pm.me>
2021-06-26 23:49:44 +0800

    shuffle review

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import json, random
 
 # terminal output colors
 class bcolors:
@@ -25,29 +25,33 @@ def judge(problem_list, requested_desc: str):
 
 
 def list_problem():
-	chapter = input("Please choose the chapter: ")
-	for i in problem_list[chapter]:
-		print(i['topic'])
-		for j in i['options']:
-			if i['answer'] == j[:1]:
-				print(f"{bcolors.OKGREEN}{j}{bcolors.ENDC}")
-			else: 
-				print(j)
-		print('')
+    chapter = input("Please choose the chapter: ")
+    for i in problem_list[chapter]:
+        print(i['topic'])
+        for j in i['options']:
+            if i['answer'] == j[:1]:
+                print(f"{bcolors.OKGREEN}{j}{bcolors.ENDC}")
+            else:
+                print(j)
+        print('')
 
 
 def review_problem():
-	chapter = input("Please choose the chapter: ")
-	for i in problem_list[chapter]:
-		print(i['topic'])
-		for j in i['options']:
-			print(j)
-		answer = input("Please fill in your answer: ")
-		if answer.lower() == i['answer'].lower():
-			print(f"{bcolors.OKGREEN}Correct!{bcolors.ENDC}")
-		else:
-			print(f"{bcolors.FAIL}Not correct. Correct answer: {i['answer']}{bcolors.ENDC}")
-		print('')
+    chapter = input("Please choose the chapter: ")
+    rand_list = random.sample(range(0, len(problem_list[chapter])), len(problem_list[chapter]))
+    rand_chap_problem_list = list()
+    for i in range(0, len(problem_list[chapter])):
+        rand_chap_problem_list.append(problem_list[chapter][rand_list[i]])
+    for i in rand_chap_problem_list:
+        print(i['topic'])
+        for j in i['options']:
+            print(j)
+        answer = input("Please fill in your answer: ")
+        if answer.lower() == i['answer'].lower():
+            print(f"{bcolors.OKGREEN}Correct!{bcolors.ENDC}")
+        else:
+            print(f"{bcolors.FAIL}Not correct. Correct answer: {i['answer']}{bcolors.ENDC}")
+        print('')
 
 
 with open('problem_list.json') as f:
@@ -56,8 +60,8 @@ with open('problem_list.json') as f:
     if command == 'R' or command == 'r':
         review_problem()
     elif command == 'L' or command == 'l':
-    	list_problem()
+        list_problem()
     elif command == 'C' or command == 'c':
         while True:
-        	requested_desc = input("Paste your problem description here: ")
-        	judge(problem_list, requested_desc)
+            requested_desc = input("Paste your problem description here: ")
+            judge(problem_list, requested_desc)
